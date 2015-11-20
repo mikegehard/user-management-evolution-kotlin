ca053e7b6c3312d8adb85b6d17ba2689f0f115af
Mike Gehard <mike.gehard@gmail.com>
2015-11-20 08:01:18 -0700

    Use bootRun for both apps and platform
    This means no compilation step.

--- start-platform.sh
+++ start-platform.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew platform/circuitBreakerDashboard:build > /dev/null
-./gradlew platform/configServer:build > /dev/null
-./gradlew platform/serviceDiscovery:build > /dev/null
-
-java -jar platform/circuitBreakerDashboard/build/libs/hystrix-dashboard-0.0.1-SNAPSHOT.jar > logs/hystrix.log 2>&1 &
-java -jar platform/configServer/build/libs/config-server-0.0.1-SNAPSHOT.jar > logs/config-server.log 2>&1 &
-java -jar platform/serviceDiscovery/build/libs/service-discovery-0.0.1-SNAPSHOT.jar > logs/service-discovery.log 2>&1 &
+./gradlew platform/circuitBreakerDashboard:bootRun > logs/hystrix.log 2>&1 &
+./gradlew platform/configServer:bootRun > logs/config-server.log 2>&1 &
+./gradlew platform/serviceDiscovery:bootRun > logs/service-discovery.log 2>&1 &
--- stop-apps.sh
+++ stop-apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Using [] will prevent the grep process from showing up in the list.
-apps=`ps | grep "[b]ootRun" | awk '{print $1}'`
+apps=`ps | grep "[a]pplications/.*:bootRun" | awk '{print $1}'`
 
 for app in $apps; do
      kill $app > /dev/null
