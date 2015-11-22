bd777110efe50cb000a51970fdb6221b79569ff5
Mike Gehard <mike.gehard@gmail.com>
2015-11-22 14:44:13 -0700

    Switch the billing configuration over to Kotlin
    The big takeaway here is @Configuration classes must be open for subclassing and all @Bean
    methods must be public and overrideable.

--- applications/billing/src/main/kotlin/com/example/billing/Application.kt
+++ applications/billing/src/main/kotlin/com/example/billing/Application.kt
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+// This class must not be final or Spring Boot is not happy.
 open class Application {
     companion object {
         @JvmStatic public fun main(args: Array<String>) {
