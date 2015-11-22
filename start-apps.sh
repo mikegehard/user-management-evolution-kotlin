5ea14c0bf2ad6a6992d05b8bb6fb455b615c6ec0
Mike Gehard <mike.gehard@gmail.com>
2015-11-22 14:10:49 -0700

    Make sure logs dir exists

--- start-apps.sh
+++ start-apps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir -p logs/
 
 ./gradlew applications/billing:clean applications/billing:bootRun > logs/billing.log 2>&1 &
 ./gradlew applications/email:clean applications/email:bootRun > logs/email.log 2>&1 &
--- start-platform.sh
+++ start-platform.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p logs/
+
 java -jar platform/circuitBreakerDashboard/build/libs/hystrix-dashboard-0.0.1-SNAPSHOT.jar > logs/hystrix.log 2>&1 &
 java -jar platform/configServer/build/libs/config-server-0.0.1-SNAPSHOT.jar > logs/config-server.log 2>&1 &
 java -jar platform/serviceDiscovery/build/libs/service-discovery-0.0.1-SNAPSHOT.jar > logs/service-discovery.log 2>&1 &
