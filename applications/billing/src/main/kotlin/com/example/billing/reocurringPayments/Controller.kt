0f8bbbe3f72e8196eece07817f1f8976c622697f
Mike Gehard <mgehard@pivotal.io>
2015-11-23 17:24:41 -0700

    Copy/paste payment controller
    Let IntelliJ do the translation for you.
    
    Some interesting parts:
    * @RequestMapping methods are an array. Kotin forces you to declare it as an array.
    * @Autowired instance variables must be var since they are not initialized in the constructor
    * @Autowired instance variables must be decalred nullable and accessed with the double bang '!!' operator

---
+++ applications/billing/src/main/kotlin/com/example/billing/reocurringPayments/Controller.kt
@@ -0,0 +1,42 @@
+package com.example.billing.reocurringPayments
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.metrics.CounterService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+    @Autowired
+    private var paymentGateway: com.example.payments.Gateway? = null
+
+    @Autowired
+    private var counter: CounterService? = null
+
+    @Autowired
+    private var service: Service? = null
+
+    @RequestMapping(value = "/reocurringPayment", method = arrayOf(RequestMethod.POST))
+    fun createReocurringPayment(@RequestBody data: Map<String, Any>): ResponseEntity<String> {
+        val responseHeaders = HttpHeaders()
+        responseHeaders.add("content-type", MediaType.APPLICATION_JSON.toString())
+
+        service!!.thisMayFail()
+
+        val response: ResponseEntity<String>
+        if (paymentGateway!!.createReocurringPayment(data["amount"] as Int)) {
+            counter!!.increment("billing.reocurringPayment.created")
+            response = ResponseEntity("{\"errors\": []}", responseHeaders, HttpStatus.CREATED)
+        } else {
+            response = ResponseEntity("{\"errors\": [\"error1\", \"error2\"]}", responseHeaders, HttpStatus.BAD_REQUEST)
+        }
+
+        return response
+    }
+}
