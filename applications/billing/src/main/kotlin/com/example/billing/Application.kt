a9045e1968193fc70b4c43a330fde265b3325f7a
Mike Gehard <mike.gehard@gmail.com>
2015-12-22 10:28:34 -0700

    No need for open keyword if you are not a @Configuration

--- applications/billing/src/main/kotlin/com/example/billing/Application.kt
+++ applications/billing/src/main/kotlin/com/example/billing/Application.kt
@@ -1,15 +1,16 @@
 package com.example.billing
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-// This class must not be final or Spring Boot is not happy.
-open class Application {
+class Application {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
