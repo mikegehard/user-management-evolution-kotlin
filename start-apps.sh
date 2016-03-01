83883fee6dac3cb06e10bd6a510403cbe3e1ef62
Mike Gehard <mike.gehard@gmail.com>
2016-03-01 15:43:18 -0700

    Put app logs in a different folder so it is easier to tail them separately

--- start-apps.sh
+++ start-apps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p logs/
+mkdir -p logs/applications
 
-./gradlew applications/billing:clean applications/billing:bootRun > logs/billing.log 2>&1 &
-./gradlew applications/email:clean applications/email:bootRun > logs/email.log 2>&1 &
-./gradlew applications/ums:clean applications/ums:bootRun > logs/ums.log 2>&1 &
+./gradlew applications/billing:clean applications/billing:bootRun > logs/applications/billing.log 2>&1 &
+./gradlew applications/email:clean applications/email:bootRun > logs/applications/email.log 2>&1 &
+./gradlew applications/ums:clean applications/ums:bootRun > logs/applications/ums.log 2>&1 &
